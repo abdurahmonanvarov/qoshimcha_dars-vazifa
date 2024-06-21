7abed78a3403008ceb9402728cac0de1731e88f2
abdurahmonanvarov <dousolo9@gmail.com>
2024-06-21 13:44:44 +0500

    last

---
+++ inde.js
@@ -0,0 +1,207 @@
+//ASOSOIY METODLAR
+//1-masala
+// function findUzunlik(arr){
+//     return arr.length;
+// }
+// console.log(findUzunlik([1,2,3,4,5]));
+
+//2-masala
+// function changeStriing(arr){
+//     return arr.toString();
+// }
+// console.log(changeStriing([1,2,3,4,5]));
+
+//3-masala
+// function useJoin(arr){
+//     return arr.join(',');
+// }
+// console.log(useJoin([1,2,3,4,5]));
+
+//4-masala
+// function addelement(arr, element){
+//     arr.push(element);
+//     return arr;
+// }
+// console.log(addelement([1,2,3,4,5],3));
+
+//5-masala
+// function deletelement(arr){
+//     arr.pop();
+//     return arr;
+// }
+// console.log(deletelement([1,2,3,4,5]));
+
+//6-masala
+// function deletelement(arr){
+//     arr.shift();
+//     return arr;
+// }
+// console.log(deletelement([1,2,3,4,5]));
+
+//7-masala
+// function addelement(arr, element){
+//     arr.unshift(element);
+//     return arr;
+// }
+// console.log(addelement([1,2,3,4,5],'salom'));
+
+//8-masala
+// function addMassive(arr, arr1){
+//     let res = arr.concat(arr1);
+//     return res;
+// }
+// console.log(addMassive([1,2,3,4,5],['salom', 'hayir']));
+
+//9-masala
+// function addMassive(arr, element, element1, element2){
+//     arr.splice(element, element1, element2);
+//     return arr;
+// }
+// console.log(addMassive([1,2,3,4,5],1,2,1505));
+
+//10-masala
+// function cutArr(arr, element, element1){
+//     let res = arr.slice(element, element1);
+//     return res;
+// }
+// console.log(cutArr([1,2,3,4,5,6],2,4));
+
+//11-masala
+// function findIndex(arr){
+//     return arr.length;
+// }
+// console.log(findIndex([1,2,3,4,5,6,7,8,9,8,7]));
+
+//12-masala
+// function findIndex(arr){
+//     return  arr.toString();
+// }
+// console.log(findIndex([1,2,3,4,5,6,7,8,9,8,7]));
+
+//13-masala
+// function joinUse(arr){
+//     return  arr.join('');
+// }
+// console.log(joinUse([1,2,3,4,5,6,7,8,9,8,7]));
+
+//14-masala
+// function joinUse(arr, element, element1){
+//     arr.push(element);
+//     arr.push(element1)
+//     return  arr;
+// }
+// console.log(joinUse([1,2,3,4,5,6,7,8,9,8,7], true, 'dunyo'));
+
+//15-masala
+// function deletelement(arr){
+//     arr.pop();
+//     arr.pop()
+//     return  arr;
+// }
+// console.log(deletelement([1,2,3,4,5,6,7,8,9,8,7]));
+
+//16-masala
+// function deletelement(arr){
+//     arr.shift();
+//     arr.shift()
+//     return  arr;
+// }
+// console.log(deletelement([1,2,3,4,5,6,7,8,9,8,7]));
+
+//17-masala
+// function joinUse(arr, element, element1) {
+//   arr.unshift(element);
+//   arr.unshift(element1);
+//   return arr;
+// }
+
+// console.log(joinUse([1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7], true, "dunyo"));
+
+//18-masala
+// function addMassive(arr, arr1, arr2) {
+//     let res = arr.concat(arr1, arr2)
+//     return res;
+//   }
+
+//   console.log(addMassive([1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7],[ true, "dunyo"],[123,456]));
+
+//19-masala
+// function addMassive(arr, element) {
+//   arr.splice(element,1);
+//   return arr;
+// }
+// console.log(addMassive([1, 2, 3, 4, 5], 3));
+
+
+//20-masala
+// function addMassive(arr, element) {
+//     let res = arr.slice(element);
+//     return res;
+//   }
+//   console.log(addMassive([1, 2, 3, 4, 5], 1));
+
+
+//SERCH METODI
+
+
+//1-MASALA
+// function findElement(arr, element){
+//  let res = arr.includes(element);
+//  return res;
+// }
+// console.log(findElement([1,2,3,4,5],4));
+
+//2-masala
+// function findIndex(arr, element){
+//     let res = arr.indexOf(element);
+//     return res;
+//    }
+//    console.log(findIndex([1,2,3,4,5],100));
+
+//3-masala
+// function findIndex(arr, element){
+//     let res = arr.lastIndexOf(element);
+//     return res;
+//    }
+//    console.log(findIndex([1,2,3,4,5],50));
+
+//4-masala
+// function findIndex(arr, element) {
+//   arr.find(function (value, index, ar) {
+//     return index > element;
+//   });
+// }
+// console.log(findIndex([1, 2, 3, 4, 5], 3));
+
+//6-masala
+// function findIndex(arr, element) {
+//     let res = arr.includes(element);
+//     return res;
+//   }
+//   console.log(findIndex([1, 2, 3, 4, 5, 'salomlar'], 'salomlar'));
+
+//7-masala
+// function findIndex(arr, element) {
+//     let res = arr.lastIndexOf(element);
+// if (arr = true){
+//     res = "Element topadi"
+// }else if (arr = false){
+//     res = "Element topmaydi"
+// }
+// return res;
+//   }
+//   console.log(findIndex([1, 2, 3, 4, 5, 'salomlar'], 'salomlar'));
+
+//8-masala
+// function findIndex(arr, element) {
+//   let res = arr.lastIndexOf(element);
+//   if ((arr = true)) {
+//     res = "Element topadi";
+//   } else if ((arr = false)) {
+//     res = "Element topmaydi";
+//   }
+//   return res;
+// }
+// console.log(findIndex([1, 2, 3, 4, 5, "salomlar"], 4));
+
+//9-masala
